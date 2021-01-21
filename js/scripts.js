b1ce92205d16165d55fd8ffbcd7990735bcdcdb1
lars-ree <70200269+lars-ree@users.noreply.github.com>
2021-01-21 12:36:58 +0100

    f

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+// Question 1
+
+const cat = {
+    complain: function() {
+        console.log("Meaow");
+    }
+}
+
+cat.complain();
+
+
+// Question 2
+
+const heading = document.querySelector("h3");
+heading.innerHTML = "Updated heading";
+
+// Question 3
+
+heading.style.fontSize = "2em";
+
+// Question 4
+
+heading.className = "subheading";
+
+// Question 5
+
+ var paragraphs = document.querySelector(" p");
+
+//paragraphs.style.fontSize = "2em"; 
